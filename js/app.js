cb63092f81d327ee25b3c9bfd14cd18cdfc500f8
Dromen2001 <dromen2001@gmail.com>
2024-05-21 12:40:10 -0700

    building functionality for ivy game play incluing un hidding section

---
+++ js/app.js
@@ -0,0 +1,24 @@
+console.log('app is running');
+
+const navigationEle = document.getElementById('navigation');
+const spielEle = document.getElementById('spiel-section');
+const startBtnEle = document.getElementById('start-game');
+const investigateBtnEle = document.getElementById('investigate');
+const rulesSectionEle = document.getElementById('rules-section');
+const villainIvyEle = document.getElementById('villain-ivy');
+const ivyQuestionEle = document.getElementById('quizz-question');
+
+startBtnEle.addEventListener('click', () => {
+  // console.log('I have been clicked')
+  villainIvyEle.removeAttribute('hidden');
+  villainIvyEle.style.color = '#ff035b';
+  villainIvyEle.style.backgroundColor = '#142e13';
+  ivyQuestionEle.innerText = 'I have made a hybrid plant that is part carnivorous and part poisonous. Based on the Latin Genus name of this plant, what are the plants that went into making the hybrid?'
+  
+});
+
+
+
+
+// console.log(rulesSectionEle);
+// console.log(spielEle);
