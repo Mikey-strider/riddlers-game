0db470fd0164e4ec58f562c38634a526b2272547
Dromen2001 <dromen2001@gmail.com>
2024-05-21 16:43:56 -0700

    apended winning and losing notice to page. Plus secret code for winner to solve

--- js/app.js
+++ js/app.js
@@ -16,19 +16,23 @@ startBtnEle.addEventListener('click', () => {
   villainIvyEle.removeAttribute('hidden');
   villainIvyEle.style.color = '#ff035b';
   villainIvyEle.style.backgroundColor = '#142e13';
+  ivyQuestionEle.style.backgroundColor = '#142e13'
   ivyQuestionEle.innerText = 'I have made a hybrid plant that is part carnivorous and part poisonous. Based on the Name of this plant (Deadly Son), what are the plants that went into making the hybrid? The answers will be in latin. Good luck!'
   plantImageEle.removeAttribute('hidden');
+  selectAnswer.removeAttribute('hidden');
 });
 
 selectAnswer.addEventListener('click', (e) => {
   // console.dir(e.target.id)
   if(e.target.id === 'answer4'){
     const correctChoice = document.createElement('h4');
-    correctChoice.innerText = 'You have chosen correctly!'; 
+    correctChoice.innerText = 'You have chosen correctly! merit sky ride'; 
+    villainIvyEle.appendChild(correctChoice);
     console.log(correctChoice);
   } else {
     const wrongChoice = document.createElement('h4');
     wrongChoice.innerText = 'You have chosen wrong, better luck next time!';
+    villainIvyEle.appendChild(wrongChoice);
     console.log(wrongChoice);
   }
 })
